dcb2934ab6ef812fba3b225cb4761bd10afb40c1
Arun Varma <varmaarun580@gmail.com>
2026-04-27 23:54:42 +0530

    size of currency dropdown menu fixed

--- src/components/CurrencyDropdown.jsx
+++ src/components/CurrencyDropdown.jsx
@@ -23,7 +23,7 @@ const CurrencyDropdown = ({ currency, onChange }) => {
       <select
         value={currency}
         onChange={(e) => onChange?.(e.target.value)}
-        className="appearance-none flex items-center gap-1.5 bg-white border border-slate-200
+        className="w-21 appearance-none flex items-center gap-1.5 bg-white border border-slate-200
                    rounded-xl px-2 sm:px-4 md:px-5 py-2 sm:py-3 md:py-4 pr-8 font-semibold text-xs sm:text-sm md:text-base text-slate-800
                    cursor-pointer hover:border-slate-300 transition-colors"
       >
@@ -36,7 +36,7 @@ const CurrencyDropdown = ({ currency, onChange }) => {
       </select>
 
      
-      <div className="pointer-events-none absolute right-4 top-1/2 -translate-y-1/2 text-slate-600 text-xs">
+      <div className="pointer-events-none absolute right-4 top-1/2 -translate-y-1/2 text-slate-600  text-xs">
         {/* Custom dropdown arrow indicator */}
         ▼
       </div>
